1cab0dbf67e0af5a3f49cff17ae11b5280f96382
Ruan <rugcosta1234@gmail.com>
2024-01-12 22:31:30 -0300

    Add: Adicioando aruivos necessários para a inicialização do projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <button className="bg-blue-500 px-2 rounded">submit</button>
+    </>
+  );
+}
+
+export default App;
