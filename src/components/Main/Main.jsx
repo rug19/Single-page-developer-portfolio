9bef5c65317d33ce33cbe5facdb59b8b50eea815
Ruan <rugcosta1234@gmail.com>
2024-02-21 21:32:22 -0300

    estilizando o titulo e paragrafo da main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,13 +9,18 @@ export default function Main() {
           src="/public/images/image-profile-mobile.webp"
           alt="image-mobile"
         />
-        <h1 className={styles.titulo}>
-          <span className={styles.upperTitulo}>Nice to meet you!</span> I'm <span>Adam Keyes.</span>
-        </h1>
-        <p className={styles.paragraphMain}>Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
-        <h3>CONTACT ME</h3>
+        <div>
+          <h1 className={styles.titulo}>
+            <span className={styles.upperTitulo}>Nice to meet you!</span> I’m{" "}
+            <span>Adam Keyes.</span>
+          </h1>
+          <p className={styles.paragraphMain}>
+            Based in the UK, I’m a front-end developer passionate about building
+            accessible web apps that users love.
+          </p>
+          <h3 className={styles.subTitle}>CONTACT ME</h3>
+        </div>
       </div>
     </main>
   );
 }
-
